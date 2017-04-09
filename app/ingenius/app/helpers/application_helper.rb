b57469f97d266877684547ecda2ecae0d5f3346f
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2017-04-09 15:28:48 -0400

    adding some helpers

--- app/ingenius/app/helpers/application_helper.rb
+++ app/ingenius/app/helpers/application_helper.rb
@@ -8,6 +8,18 @@ module ApplicationHelper
     @categories = Category.all
   end
 
+  def alert_type(name)
+    if name=='notice'
+      return 'info'
+    elsif name == 'alert'
+      return 'danger'
+    end
+  end
+
+  def twitter_share(event)
+    "http://twitter.com/home?status=Iré al #{event.title} @ #{event.schedules.first.date.strftime("%d/%m/%Y")} #{event.schedules.first.date.strftime("%H:%M")} #{event_url(event)}"
+  end
+
 
 
 end
